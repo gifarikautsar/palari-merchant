4f11f05c752e1323e8cff26002e6abb63621283f
gifarikautsar <gifari13@gmail.com>
2015-06-30 14:24:03 +0700

    Edit route

--- app/app.routes.js
+++ app/app.routes.js
@@ -2,7 +2,7 @@ var routesApp = angular.module('routesApp', ['ui.router']);
 
 routesApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {  
 
-  $urlRouterProvider.otherwise('/login');
+  $urlRouterProvider.otherwise('/404');
 
 
   // $locationProvider.html5Mode(true);
